d3aa73f0e344a35d2c166c909281a68d5ba20c1b
Ashik Imman <ashikimman007@gmail.com>
2024-09-10 11:29:41 +0530

    Update scrips.js

--- scrips.js
+++ scrips.js
@@ -1,13 +1,1 @@
-function trackShipment() {
-    var trackingNumber = document.getElementById("tracking-number").value;
-    var resultDiv = document.getElementById("tracking-result");
 
-    // Example response (You can replace this with an actual API call to get tracking information)
-    if (trackingNumber === "123456789") {
-        resultDiv.innerHTML = "<p>Your shipment is on the way! Estimated delivery: 2 days.</p>";
-    } else if (trackingNumber === "") {
-        resultDiv.innerHTML = "<p>Please enter a tracking number.</p>";
-    } else {
-        resultDiv.innerHTML = "<p>Tracking number not found. Please try again.</p>";
-    }
-}
